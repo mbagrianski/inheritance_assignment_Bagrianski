89a290a8198f02b127cb358bb5514facb50cfea1
mbagrianski <46272573+mbagrianski@users.noreply.github.com>
2019-10-09 09:54:49 -0400

    Update Parabola.java

--- src/sample/Parabola.java
+++ src/sample/Parabola.java
@@ -1,7 +1,7 @@
-package sample;
+package application;
 
 public class Parabola extends Quadratic implements Calculations{
-
+	
 	protected double a;
 	protected double b;
 	protected double x1;
@@ -11,7 +11,11 @@ public class Parabola extends Quadratic implements Calculations{
 		this.a = a;
 		this.b = b;
 		this.x1 = x1;
+		super.setName("Parabola"); 
+}
 
+	@Override
+	public String toString() {
 		String stra = new String(String.valueOf(a));
 		if(a == 1.0){
 			stra = "";
@@ -19,7 +23,9 @@ public class Parabola extends Quadratic implements Calculations{
 			stra = "-";
 		}
 		String strb = new String(String.valueOf(b));
-		if(b == 0.0){
+		if (b == 0 && a == 0) {
+			strb = "0";
+		}else if(b == 0) {
 			strb = "";
 		}else if(b > 0.0){
 			strb = "+ " + strb;
@@ -34,12 +40,12 @@ public class Parabola extends Quadratic implements Calculations{
 		}
 
 		if (a == 0){
-			super.name = "f(x) = " + strb;
+			return "f(x) = " + strb;
 		}else{
-			super.name = "f(x) = " + stra + "(x" + strx1 + ")^2 " + strb;
+			return "f(x) = " + stra + "(x" + strx1 + ")^2 " + strb;
 		}
 	}
-
+	
 	@Override
 	public double val(double x) {
 		double y = a * (x - x1) * (x - x1) + b;
@@ -62,14 +68,14 @@ public class Parabola extends Quadratic implements Calculations{
 		for(double i = x_start; i <= x_end; i+= deltaX) {
 			area += val(i) * deltaX;
 		}
-		return area;
+		return area;	
 	}
 
 	@Override
 	public double getSlope(double x) {
 		double deltaX = 0.01;
-		double slope = (val(x + deltaX) - val(x - deltaX)) / (2 * deltaX);
-		return slope;
+		double slope = (val(x + deltaX) - val(x - deltaX)) / (2 * deltaX);	
+		return slope;		
 	}
 
 }
