4430aa8378a0cb98718305e9ec52d23b2af1bab9
mbagrianski <46272573+mbagrianski@users.noreply.github.com>
2019-10-09 09:55:28 -0400

    Update Cubic.java

--- src/sample/Cubic.java
+++ src/sample/Cubic.java
@@ -1,7 +1,7 @@
-package sample;
+package application;
 
 public class Cubic extends Function implements Calculations{
-
+	
 	protected double a;
 	protected double b;
 	protected double c;
@@ -14,8 +14,64 @@ public class Cubic extends Function implements Calculations{
 		this.b = b;
 		this.c = c;
 		this.d = d;
-		this.x1 = x1;
-		// TODO Auto-generated constructor stub
+		this.x1 = x1;	
+		super.setName("Cubic"); 
+	}
+	
+	@Override
+	public String toString() {
+		String stra = new String(String.valueOf(a));
+		if(a == 1.0){
+			stra = "";
+		}else if (a == -1.0){
+			stra = "-";
+		}
+		String strb = new String(String.valueOf(b));
+		if(a == 1.0){
+			strb = "";
+		}else if (a == -1.0){
+			strb = "-";
+		}
+		String strc = new String(String.valueOf(c));
+		if(a == 1.0){
+			strc = "";
+		}else if (a == -1.0){
+			strc = "-";
+		}
+		String strd = new String(String.valueOf(d));
+		if (d == 0 && c == 0 && b == 0 && a == 0) {
+			strd = "0";
+		}else if(d == 0) {
+			strd = "";
+		}else if(d > 0.0){
+			strd = "+ " + strd;
+		}
+		String strx1 = new String(String.valueOf(-x1));
+		if(x1 == 0.0){
+			strx1 = "";
+		}else if (x1 < 0.0){
+			strx1 = " + " + strx1;
+		}else{
+			strx1 = " " + strx1;
+		}
+
+		if (a == 0 && b ==0 && c == 0){		
+			return "f(x) = " + strd;
+		}else if(a == 0 && b == 0){
+			return "f(x) = " + strc + "(x" + strx1 + ") " + strd;
+		}else if(b == 0 && c == 0) {
+			return "f(x) = " + stra + "(x" + strx1 + ")^3 " + strd;
+		}else if(a == 0 && c == 0) {
+			return "f(x) = " + strb + "(x" + strx1 + ")^2 " + strd;
+		}else if(a == 0){
+			return "f(x) = " + strb + "(x" + strx1 + ")^2 "+ strc + "(x" + strx1 + ") " + strd;
+		}else if(b == 0){
+			return "f(x) = " + stra + "(x" + strx1 + ")^3 "+ strc + "(x" + strx1 + ") " + strd;
+		}else if(c == 0){
+			return "f(x) = " + stra + "(x" + strx1 + ")^3 "+ strb + "(x" + strx1 + ")^2 " + strd;
+		}else {
+			return "f(x) = " + stra + "(x" + strx1 + ")^3 "+ strb + "(x" + strx1 + ")^2 " + strc + "(x" + strx1 + ")^2 " + strd;
+		}
 	}
 
 	@Override
@@ -41,14 +97,14 @@ public class Cubic extends Function implements Calculations{
 		for(double i = x_start; i <= x_end; i+= deltaX) {
 			area += val(i) * deltaX;
 		}
-		return area;
+		return area;	
 	}
 
 	@Override
 	public double getSlope(double x) {
 		double deltaX = 0.01;
-		double slope = (val(x + deltaX) - val(x - deltaX)) / (2 * deltaX);
-		return slope;
+		double slope = (val(x + deltaX) - val(x - deltaX)) / (2 * deltaX);	
+		return slope;		
 	}
 
 }
