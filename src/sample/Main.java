40e590042468df4c0627a279b05c73774b3f0135
mbagrianski <46272573+mbagrianski@users.noreply.github.com>
2019-10-09 09:53:02 -0400

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package application;
 
 import javafx.application.Application;
 import javafx.scene.Group;
@@ -15,45 +15,47 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		
+		Linear l = new Linear(0, 0, 0); //parameters		
+		Arc a = new Arc(0, 0, 0); //parameters
+		Logarithm ln = new Logarithm(0, 0, 0); //parameters		
+		Quadratic q = new Quadratic(-10, -12, -14, -16); //parameters		
+		Cubic c = new Cubic(0, 0, 0, 0, 0); //parameters
+		Parabola p = new Parabola(-1, 0, 0); //parameters
+		
+		
+		System.out.println(l.toString());	
+		System.out.println(l.getName());
+		
 
-        Linear l = new Linear(-2, -1, 0); //parameters
-        System.out.println(l.toString());
-        System.out.println(l.getArea(-10, 10));
-        Arc a = new Arc(0, 0, 0); //parameters
-        Logarithm log = new Logarithm(0, 0, 0); //parameters
-        Quadratic q = new Quadratic(10, 0, 0, 0); //parameters
-        Cubic c = new Cubic(0, 0, 0, 0, 0); //parameters
-        Parabola p = new Parabola(0, 0, 0); //parameters
+		launch(args);
 
-        launch(args);
+	}
 
-    }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Drawing Functions Test");
+		Group root = new Group();
+		Canvas canvas = new Canvas(300, 250);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		drawShapes(gc);
+		root.getChildren().add(canvas);
+		primaryStage.setScene(new Scene(root));
+		primaryStage.show();
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Drawing Functions Test");
-        Group root = new Group();
-        Canvas canvas = new Canvas(300, 250);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        drawShapes(gc);
-        root.getChildren().add(canvas);
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+	}
 
-    }
-
-    // test method for drawing - you can use this as an example for drawing various types of lines
-    private void drawShapes(GraphicsContext gc) {
-        gc.setFill(Color.GREEN);
-        gc.setStroke(Color.BLUE);
-        gc.setLineWidth(5);
-        gc.strokeLine(40, 10, 10, 40);
-
-        gc.strokePolygon(new double[]{60, 90, 60, 90},
-                new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolyline(new double[]{110, 140, 110, 140},
-                new double[]{210, 210, 240, 240}, 4);
-    }
+	// test method for drawing - you can use this as an example for drawing various types of lines
+	private void drawShapes(GraphicsContext gc) {
+		gc.setFill(Color.GREEN);
+		gc.setStroke(Color.BLUE);
+		gc.setLineWidth(5);
+		gc.strokeLine(40, 10, 10, 40);
+		gc.strokePolygon(new double[]{60, 90, 60, 90},
+				new double[]{210, 210, 240, 240}, 4);
+		gc.strokePolyline(new double[]{110, 140, 110, 140},
+				new double[]{210, 210, 240, 240}, 4);
+	}
 
 }
